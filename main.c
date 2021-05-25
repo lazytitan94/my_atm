970a2aa25d8cac5d0ad886ccbb995b4e69c48bf1
lazytitan94 <chrisalvanos17@gmail.com>
2021-05-25 15:14:38 +0300

    final

--- main.c
+++ main.c
@@ -8,24 +8,29 @@ int main()
     int pin, pinat;
     int choice;
     char cont ='y';
+    char user[20];
 
     printf("************************\n");
-    printf("**Welcome to eCorp ATM**\n"); // evlepa Mr.Robot otan to egrafa
+    printf("**Welcome to eCorp ATM**\n"); // έβλεπα Mr.Robot όταν το έγραφα
     printf("************************\n");
 
 
+    sleep(3);
+    printf("Please type in your name: \n"); // ζητάω όνομα έτσι ώστε να αναφέρεται το πρόγραμμα στο χρήστη με όνομα (για ομορφιά)
+    scanf("%s",user);
 
-
-    printf("Please type your PIN:\n");
+    printf("Please type your PIN %s",user); // Το PIN είναι 1234
+    printf(".\n");
     scanf("%d",&pin);
 
     if (pin == 1234)
     {
         system("cls");
-        printf("Welcome Chris.\n\n\n");
+        printf("Welcome %s",user);
+        printf(".\n\n\n");
 
 
-        sleep(2);
+        sleep(3);
 
         while(cont!='n' && choice!=5)
         {
@@ -34,36 +39,39 @@ int main()
         printf("2.Credit balance \n");
         printf("3.Debit balance \n");
         printf("4.See total balance \n");
-        printf("5.Exit \n");
+        printf("5.Exit \n\n\n");
         printf("Please select an option: \n");
                     scanf("%d",&choice);
 
                 switch(choice)
                 {
                     case 1:
+                    sleep(2);
                     printf("Please type in your new balance: \n");
                     scanf("%f",&balance);
                     sleep(2);
                     printf("Your new balance is: %.2f Euro.\n",balance);
+                    sleep(2);
                     printf("Do you want to continue? y to continue n to exit.\n");
                     scanf("%s",&cont);
                     system("cls");
                     break;
 
                 case 2:
-
+                    sleep(2);
                     printf("Please type the amount you want to deposit: \n");
                     scanf("%f",&deposit);
                     sleep(2);
                     balance = balance + deposit;
                     printf("Your new balance is: %.2f Euro.\n",balance);
+                    sleep(2);
                     printf("Do you want to continue? y to continue n to exit.\n");
                     scanf("%s",&cont);
                     system("cls");
                     break;
 
                 case 3:
-
+                    sleep(2);
                     printf("Please type in the amount you want to withdraw: \n");
                     scanf("%f",&withdraw);
                     sleep(2);
@@ -71,12 +79,14 @@ int main()
                     if (balance < withdraw)
                     {
                         printf("You don't have enough money to withdraw.\n");
+                        sleep(2);
                         printf("Do you want to continue? y to continue n to exit.\n");
                         scanf("%s",&cont);
                         system("cls");
                         break;
                     }
                     printf("Your new balance is: %.2f Euro.\n",balance);
+                    sleep(2);
                     printf("Do you want to continue? y to continue n to exit.\n");
                     scanf("%s",&cont);
                     system("cls");
@@ -84,8 +94,9 @@ int main()
 
 
                 case 4:
-
+                    sleep(2);
                     printf("Your balance is: %.2f Euro.\n",balance);
+                    sleep(2);
                     printf("Do you want to continue? y to continue n to exit.\n");
                     scanf("%s",&cont);
                     system("cls");
